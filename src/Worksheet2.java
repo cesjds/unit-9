02d7974c906824549b81e3c32d4e9bd5fd22c356
Yoav Amit <yoavamit@icloud.com>
2022-05-12 15:36:21 -0400

    Add power method to Worksheet2

--- src/Worksheet2.java
+++ src/Worksheet2.java
@@ -1,17 +1,20 @@
 public class Worksheet2 {
     public static void main(String[] args) {
-        // num is the variable used for testing the methods
+        // the variables used for testing the methods
         int num = 12;
+        double base = 2.0;
+        int exp = 3;
 
         // Calling the methods
         System.out.println(reverseNum(num) + "\n");
         System.out.println(recursiveFactorial(num) + "\n");
+        System.out.println(power(base, exp));
     }
 
 
     // Pre-condition: num is a positive integer
     // Post-condition: reverses the order of a given number
-    public static String reverseNum (int num) {
+    public static String reverseNum(int num) {
         if (num != 0) {
             String s = "" + (num % 10);
             return (s + reverseNum(num / 10));
@@ -23,10 +26,24 @@ public class Worksheet2 {
 
     // Pre-condition: n is a positive integer
     // Post-condition: the factorial will be returned
-    public static int recursiveFactorial (int num) {
+    public static int recursiveFactorial(int num) {
         if (num == 0)
             return 1;
         else
             return (num * recursiveFactorial(num - 1));
+    }
+
+
+    // Pre-condition: none
+    // Post-condition: num is raised to exp and so we get back a base raised to an exponent
+    public static double power(double base, int exp) {
+        if (base == 0)
+            return 0;
+        else if (exp == 0)
+            return 1;
+        else if (exp > 0)
+            return base * power(base, exp - 1);
+        else
+            return 1 / power(base, -exp);
     }
 }
