0fa85966ffa932180f3d0c96e7e8a674c40e1ce8
Yoav Amit <yoavamit@icloud.com>
2022-05-13 15:27:12 -0400

    Add fibonacci method to Worksheet3

--- src/Worksheet3.java
+++ src/Worksheet3.java
@@ -1,2 +1,21 @@
 public class Worksheet3 {
+    public static void main(String[] args) {
+        // the variables used for testing the method
+        int num = 6;
+
+        // Calling the method
+        System.out.println(fibonacci(num));
+    }
+
+
+    // Pre-condition: num is a positive integer
+    // Post-condition: returns the fibonacci number of num
+    public static int fibonacci (int num) {
+        if (num == 0)
+            return 0;
+        else if (num == 1)
+            return 1;
+        else
+            return fibonacci(num - 1) + fibonacci(num - 2);
+    }
 }
