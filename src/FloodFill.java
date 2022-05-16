8756ed94861fc73aa61fda3b0a7fa14d6fbfcbf6
Yoav Amit <yoavamit@icloud.com>
2022-05-16 10:12:08 -0400

    Add instructions to FloodFill class

--- src/FloodFill.java
+++ src/FloodFill.java
@@ -1,2 +1,44 @@
+/********************************************************************
+ * Written by:
+ *
+ * Period:
+ *
+ * ASSIGNMENT:  To write a program that asks the user to input the
+ * name of a datafile containing the outline of a figure. Then ask
+ * for the row and column where the user would like to begin the
+ * floodfill, use recursion to do the floodfill and then print the
+ * final figure.  You will need:
+ *
+ * -  a fill method that asks the user for a file name and reads a
+ * ‘shape’ from a data file into a 2D array of chars.
+ *
+ * -  a print method that asks the user for a starting point.
+ * If the point is inside the shape, you should recursively
+ * floodfill the shape. If the point is outside the shape the 2
+ * ‘background’ should be floodfilled.  If the point is on a line
+ * nothing should happen.  Error check the row & column to make
+ * sure they are in the 2D array.
+ * You should use a monospace font to print.
+ * You can Consolas for pc's or Menlo for macs.
+ *
+ * -  a second print method that prints the floodfilled shape
+ * and asks the user if (s)he would like to fill another shape.
+ *
+
+ * -  a recursive floodFill method that does the floodfill.
+ *
+ *
+ * The data file will be in the form:
+ *
+ * 		#rows #cols
+ * 		row col (for each point in shape)
+ * 		row col
+ * 		etc…
+ *
+ * your program should continue reading data files and floodfilling
+ * until the user wants to quit.
+ *
+ *******************************************************************/
+
 public class FloodFill {
 }
