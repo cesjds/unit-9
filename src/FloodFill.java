4e074b341fafe7803eeb1bedb89c9a4e97474809
Yoav Amit <yoavamit@icloud.com>
2022-05-17 14:11:12 -0400

    Add fillArray and printArray methods

--- src/FloodFill.java
+++ src/FloodFill.java
@@ -1,3 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.util.Locale;
+import java.util.Scanner;
+
+import static java.lang.String.valueOf;
+
 /********************************************************************
  * Written by: Yoav Amit
  *
@@ -42,43 +50,99 @@
 
 public class FloodFill {
     public static void main(String[] args) {
-
+//        char grid[][] = fillArray();
+//        printArray(grid);
+        printArray(fillArray());
     }
 
     /**
     asks the user for a file name and reads a ‘shape’ from a data file into a 2D array of chars.
      **/
-    public static char fillArray() {
+    public static char[][] fillArray() {
+        try {
+            // Prompting the user for the text file name
+            String fileName = JOptionPane.showInputDialog("Enter a text file:");
+
+            // Opening the file and confirming that the file is being read
+            Scanner inFile = new Scanner(new File("data/" + fileName.toLowerCase() + ".txt"));
+            String message = "Extracting data from: " + fileName.toLowerCase() + ".txt";
+            JOptionPane.showMessageDialog(null, message);
+
+            // Finding the amount of rows and columns
+            int rows = inFile.nextInt();
+            int columns = inFile.nextInt();
+
+            // Declaring the array with proper rows and columns
+            char grid[][] = new char[rows][columns];
+
+            // Filling the array with blank spaces
+            for (int r = 0; r < rows; r++) {
+                for (int c = 0; c < columns; c++) {
+                    grid[r][c] = ' ';
+                }
+            }
+
+            // Declaring variables to use as x and y coordinates
+            int x = inFile.nextInt();
+            int y = inFile.nextInt();
+
+            // Filling the array with '*' according to text file
+            while (inFile.hasNext()) {
+                for (int r = 0; r < rows; r++) {
+                    for (int c = 0; c < columns; c++) {
+                        if(r == x && c == y) {
+                            grid[r][c] = '*';
+                            x = inFile.nextInt();
+                            y = inFile.nextInt();
+                        }
+                    }
+                }
+            }
+
+            // Closing the file
+            inFile.close();
+
+            // Returning the array
+            return grid;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    /**
+     asks the user for a starting point.
+     If the point is inside the shape, you should recursively
+     floodfill the shape. If the point is outside the shape the 2
+     ‘background’ should be floodfilled.  If the point is on a line
+     nothing should happen.  Error check the row & column to make
+     sure they are in the 2D array.
+     **/
+    public static void printArray (char[][] grid) {
+        for (int r = 0; r < 15; r++) {
+            System.out.print("\n");
+            for (int c = 0; c < 25; c++) {
+                System.out.print(grid[r][c] + " ");
+            }
+        }
+    }
+
+
+    /**
+     a second print method that prints the floodfilled shape
+     and asks the user if (s)he would like to fill another shape.
+     **/
+    public static void printFloodFill () {
 
     }
 
 
     /**
-    asks the user for a starting point.
-    If the point is inside the shape, you should recursively
-    floodfill the shape. If the point is outside the shape the 2
-    ‘background’ should be floodfilled.  If the point is on a line
-    nothing should happen.  Error check the row & column to make
-    sure they are in the 2D array.
-    **/
-    public static void printArray() {
-
-    }
-
-
-    /**
-    a second print method that prints the floodfilled shape
-    and asks the user if (s)he would like to fill another shape.
-    **/
-    public static void printFloodFill() {
-
-    }
-
-
-    /**
-    a recursive floodFill method that does the floodfill.
-    **/
-    public static void floodFill() {
+     a recursive floodFill method that does the floodfill.
+     **/
+    public static void floodFill () {
 
     }
 }
