19de30cdb3976071a0559510d1da521127728d72
Yoav Amit <yoavamit@icloud.com>
2022-05-18 14:58:26 -0400

    Clean up code

--- src/FloodFill.java
+++ src/FloodFill.java
@@ -133,9 +133,6 @@ public class FloodFill {
             }
         }
 
-
-
-//        UIManager.put("OptionPane.minimumSize",new Dimension(600,1000));
         JOptionPane.showInputDialog(null, message);
     }
 
