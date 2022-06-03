444991a61b4ef45863c69f839f4b3bedf99bf183
Yoav Amit <yoavamit@icloud.com>
2022-06-03 11:34:17 -0400

    Create successful printArray method using SOP

--- src/FloodFill.java
+++ src/FloodFill.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.io.File;
 import java.util.Scanner;
 
-/********************************************************************
+/**
  * Written by: Yoav Amit
  *
  * Period: 1
@@ -30,7 +30,6 @@ import java.util.Scanner;
  * -  a second print method that prints the floodfilled shape
  * and asks the user if (s)he would like to fill another shape.
  *
-
  * -  a recursive floodFill method that does the floodfill.
  *
  *
@@ -44,18 +43,29 @@ import java.util.Scanner;
  * your program should continue reading data files and floodfilling
  * until the user wants to quit.
  *
- *******************************************************************/
+ **/
 
 public class FloodFill {
     public static void main(String[] args) {
         changeJOP();
-        printArray(fillArray());
+        int again = 0;
+        char[][] picture;
+
+        do {
+            picture = readIn();
+            String start = printEmptyShape(picture);
+            String[] pieces = start.split("\\s+");
+            int startRow = Integer.parseInt(pieces[0]);
+            int startCol = Integer.parseInt(pieces[1]);
+            floodFill(picture, startRow, startCol);
+            again = printFilledShape(picture);
+        } while (again == 0);
     }
 
     /**
     asks the user for a file name and reads a ‘shape’ from a data file into a 2D array of chars.
      **/
-    public static char[][] fillArray() {
+    public static char[][] readIn() {
         try {
             // Prompting the user for the text file name
 //            String fileName = JOptionPane.showInputDialog("Enter the file name:");
@@ -66,7 +76,6 @@ public class FloodFill {
             // Opening the file and confirming that the file is being read
             Scanner inFile = new Scanner(new File("data/" + fileName.toLowerCase() + ".txt"));
             String message = "Extracting data from: " + fileName.toLowerCase() + ".txt";
-//            JOptionPane.showMessageDialog(null, message);
 
             // Finding the amount of rows and columns
             int rows = inFile.nextInt();
@@ -83,20 +92,11 @@ public class FloodFill {
             }
 
             // Declaring variables to use as x and y coordinates
-            int x = inFile.nextInt();
-            int y = inFile.nextInt();
-
             // Filling the array with '*' according to text file
             while (inFile.hasNext()) {
-                for (int r = 0; r < rows; r++) {
-                    for (int c = 0; c < columns; c++) {
-                        if(r == x && c == y) {
-                            grid[r][c] = '*';
-                            x = inFile.nextInt();
-                            y = inFile.nextInt();
-                        }
-                    }
-                }
+                int x = inFile.nextInt();
+                int y = inFile.nextInt();
+                grid[x][y] = '*';
             }
 
             // Closing the file
@@ -120,44 +120,49 @@ public class FloodFill {
      nothing should happen.  Error check the row & column to make
      sure they are in the 2D array.
      **/
-    public static void printArray (char[][] grid) {
+    public static String printEmptyShape(char[][] picture) {
         String strGrid = "  ";
 
-        for (int c = 0; c < grid[0].length; c++) {
+        for (int c = 0; c < picture[0].length; c++) {
             strGrid += (c % 10 + "  ");
         }
 
-        for (int r = 0; r < grid.length; r++) {
+        strGrid += "\n";
+
+        for (int r = 0; r < picture.length; r++) {
             strGrid += (r % 10);
 
             strGrid += " ";
 
-            for (int c = 0; c < grid[0].length; c++)
-                strGrid += (grid[r][c] + "  ");
+            for (int c = 0; c < picture[0].length; c++)
+                strGrid += (picture[r][c] + "  ");
 
             strGrid += "\n";
         }
 
         // Tests the printArray method using SOP since JOP isn't using a monospace font
         System.out.println(strGrid);
-//        JOptionPane.showInputDialog(null, strGrid);
 
+        return strGrid;
     }
 
 
     /**
-     a second print method that prints the floodfilled shape
-     and asks the user if (s)he would like to fill another shape.
-     **/
-    public static void printFloodFill () {
+     * a second print method that prints the floodfilled shape
+     * and asks the user if (s)he would like to fill another shape.
+     *
+     * @return
+     */
+    public static int printFilledShape(char[][] picture) {
 
+        return 0;
     }
 
 
     /**
      a recursive floodFill method that does the floodfill.
      **/
-    public static void floodFill () {
+    public static void floodFill (char[][] picture, int startRow, int startCol) {
 
     }
 
